4355feec14ce30b11176409bfc50f14d7f9b9439
rbaron <rbaron@rbaron.net>
2021-08-07 15:03:37 +0200

    Experiments with Adafruit's NeoMatrix
    Shapes look distorted.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,88 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
 #include <Arduino.h>
 
+#define PIN 15
+
+// MATRIX DECLARATION:
+// Parameter 1 = width of NeoPixel matrix
+// Parameter 2 = height of matrix
+// Parameter 3 = pin number (most are valid)
+// Parameter 4 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the matrix; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
+//     rows or in vertical columns, respectively; pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns proceed
+//     in the same order, or alternate lines reverse direction; pick one.
+//   See example below for these values in action.
+// Parameter 5 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_GRBW    Pixels are wired for GRBW bitstream (RGB+W NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+
+// Example for NeoPixel Shield.  In this application we'd like to use it
+// as a 5x8 tall matrix, with the USB port positioned at the top of the
+// Arduino.  When held that way, the first pixel is at the top right, and
+// lines are arranged in columns, progressive order.  The shield uses
+// 800 KHz (v2) pixels that expect GRB color data.
+Adafruit_NeoMatrix matrix =
+    Adafruit_NeoMatrix(32, 8, PIN,
+                       NEO_MATRIX_TOP + NEO_MATRIX_RIGHT + NEO_MATRIX_COLUMNS +
+                           NEO_MATRIX_PROGRESSIVE,
+                       NEO_GRB + NEO_KHZ800);
+
+// Adafruit_NeoMatrix matrix =
+//     Adafruit_NeoMatrix(32, 8, PIN,
+//                        NEO_MATRIX_TOP + NEO_MATRIX_RIGHT + NEO_MATRIX_COLUMNS
+//                        +
+//                            NEO_MATRIX_PROGRESSIVE,
+//                        NEO_GRB);
+
+const uint16_t colors[] = {matrix.Color(255, 0, 0), matrix.Color(0, 255, 0),
+                           matrix.Color(0, 0, 255)};
+
 void setup() {
-  // put your setup code here, to run once:
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(10);
+  matrix.setTextColor(colors[0]);
+
+  matrix.fillScreen(0);
+  matrix.show();
+  matrix.setCursor(0, 0);
+  // matrix.print(F("Howdy"));
+  matrix.drawCircle(3, 3, 2, colors[1]);
+
+  matrix.drawRect(16, 1, 4, 4, colors[2]);
 }
 
+int x = matrix.width();
+int pass = 0;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  // matrix.fillScreen(0);
+  // matrix.setCursor(x, 0);
+  // matrix.print(F("Howdy"));
+  // if (--x < -36) {
+  //   x = matrix.width();
+  //   if (++pass >= 3) pass = 0;
+  //   matrix.setTextColor(colors[pass]);
+  // }
+
+  // matrix.fillScreen(0);
+  // if (x-- < 0) x = matrix.width();
+  // for (int i = 0; i < 8; i++) {
+  //   matrix.drawPixel(x, i, colors[0]);
+  // }
+  // matrix.drawCircle(x, 3, 2, colors[1]);
+  // portDISABLE_INTERRUPTS();
+  delay(1);
+  matrix.show();
+  // portENABLE_INTERRUPTS();
+  delay(100);
 }
